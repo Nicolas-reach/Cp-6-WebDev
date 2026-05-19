c1b182d0e6fa1b13694ae12c9f7b15d646e34d7f
Nicolas <nicolas.forcione2@gmail.com>
2026-05-19 16:09:07 +0000

    feat: cria componente Footer

---
+++ vite-project/src/components/Footer.jsx
@@ -0,0 +1,14 @@
+function Footer() {
+  return (
+    <footer className="bg-black text-white p-4 mt-10 text-center text-sm">
+      <p>© 2026 devbooks - Todos os direitos reservados</p>
+      <div className="flex gap-4 justify-center mt-2">
+        <a href="#">GitHub</a>
+        <a href="#">Twitter</a>
+        <a href="#">LinkedIn</a>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
